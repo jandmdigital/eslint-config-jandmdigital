a660bb2e14aadd63e04ea338ce1db9636d29ab95
Jim Jenkins <jimjenkins5@gmail.com>
2017-04-27 13:28:01 -0400

    fix brace style and sourcetype

--- index.js
+++ index.js
@@ -2,19 +2,25 @@
 
 module.exports = {
 
-   'extends': [ 'eslint-config-silvermine' ],
+   'extends': [ 'silvermine' ],
 
    'env': {
       'es6': true,
    },
 
+   'parserOptions': {
+      'sourceType': 'module'
+   },
+
    'rules': {
       'silvermine/no-multiline-var-declarations': 'off',
       'one-var': 'off',
+      'no-console': 'off',
       'no-restricted-syntax': 'off',
       'new-cap': 'off',
       'no-process-env': 'off',
       'strict': 'off',
       'comma-dangle': [ 'error', 'always-multiline' ],
+      'brace-style': [ 'error', '1tbs', { 'allowSingleLine': true } ],
    },
 };
